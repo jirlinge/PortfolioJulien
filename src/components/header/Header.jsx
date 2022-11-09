b2a59336465077164d549913977ca2445e11ce69
jirlinge <julienirlinger@gmail.com>
2022-11-09 14:46:41 +0100

    ajout du changement d'etat toggle pour le menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,11 +1,15 @@
 import React from "react";
+import { useState } from "react";
 import "./header.css"
 function Header() {
+    /* Toggle Menu */
+
+    const[Toggle, showMenu] = useState(false);
 return(
     <header className="header">
         <nav className="nav container">
             <a href="index.html" className="nav__logo">Julien</a>
-            <div className="nav__menu">
+            <div className ={Toggle ? "nav__menu show-menu" : "nav__menu"}>
                 <ul className="nav__list grid">
                     <li className="nav__item">
                         <a href="#home" className="nav__link active__link ">
@@ -39,9 +43,13 @@ return(
                     </li>
                 </ul>
 
-                <i class="uil uil-times nav__close"></i>
+                <i 
+                    class="uil uil-times nav__close" 
+                    onClick={() => showMenu (!Toggle)}>
+                </i>
             </div>
-            <div className="nav__toggle">
+            <div className="nav__toggle" onClick={() => showMenu
+                (!Toggle)}>
                 <i class="uil uil-apps"></i>
             </div>
         </nav>
