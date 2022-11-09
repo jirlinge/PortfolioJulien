bbd52df37d6a1db0343c366d6917abe20a46c332
jirlinge <julienirlinger@gmail.com>
2022-11-09 16:56:49 +0100

    ajout icon hi

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Social() {
+    return(
+        <>
+        <div className="home__social">
+            <a href="https://www.instagram.com/goodlinesll/" className="home__social-icon" target="_blank">
+                <i class="uil uil-instagram"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/julien-irlinger-60647091/" className="home__social-icon" target="_blank">
+                <i class="uil uil-linkedin-alt"></i>
+            </a>
+            <a href="https://github.com/jirlinge" className="home__social-icon" target="_blank">
+                <i class="uil uil-github-alt"></i>
+            </a>
+        </div>
+        </>
+    )
+}
+export default Social
