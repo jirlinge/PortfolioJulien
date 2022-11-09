9632b838b008d94cc9a86df4bd302e9ca0633f10
jirlinge <julienirlinger@gmail.com>
2022-11-09 23:28:46 +0100

    finition responsive home

---
+++ src/components/about/Info.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Info() {
+    return (
+        <div className="about__info grid">
+            {/* <div className="about__box">
+                <h3 className="about__title">Experience</h3>
+                <span className="about__subtitle">3 Years Working</span>
+            </div>
+            <div className="about__box">
+                <h3 className="about__title">Experience</h3>
+                <span className="about__subtitle">3 Years Working</span>
+            </div>
+            <div className="about__box">
+                <h3 className="about__title">Experience</h3>
+                <span className="about__subtitle">3 Years Working</span>
+            </div> */}
+        </div>
+    )
+}
+
+export default Info
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import "./home.css";
 import Social from './Social';
 import Data from './Data';
+import ScrollDown from './ScrollDown';
 
 function Home () {
     return(
         <section className="home section" id="home">
             <div className="home__container container grid">
-                <div className="home__content grd">
+                <div className="home__content grid">
                     <Social></Social>
                     <div className="home__img"></div>
                     <Data></Data>
                 </div>
+                <ScrollDown></ScrollDown>
             </div>
         </section>
     )
