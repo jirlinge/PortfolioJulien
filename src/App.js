469c0cc601d8edbb86508913430125a2db912f1b
jirlinge <julienirlinger@gmail.com>
2022-11-10 20:03:58 +0100

    qualification ecole

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Services from './components/services/Services';
+import Qualification from './components/qualitfication/Qualification';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
       <About />
       <Skills />
       <Services />
+      <Qualification />
     </main>
     </>
   );
---
+++ src/components/qualitfication/Qualification.jsx
@@ -0,0 +1,108 @@
+import React from "react";
+import "./qualification.css"
+
+function Qualification() {
+    return (
+        <section className="qualification section">
+            <h2 className="section__title">Qualification</h2>
+            <span className="section__subtitle">My personel journey</span>
+
+            <div className="qualification__container container">
+                <div className="qualification__tabs">
+                    <div className="qualification__button qualification__active button--flex">
+                        <i className="uil uil-graduation-cap qualification__icon">
+                        </i>Education
+                    </div>
+
+                    <div className="qualification__button button--flex">
+                        <i className="uil uil-briefcase-alt qualification__icon">
+                        </i>Experience
+                    </div>
+                </div>
+
+                <div className="qualification__sections">
+                    <div className="qualification__content">
+                        <div className="qualification__data">
+                            <div>
+                                <h3 className="qualification__title">Concepteur developpeur application</h3>
+                                <span className="qualification__subtitle">
+                                    Lamanu Versailles
+                                </span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt">
+                                        May 2021 - June 2022
+                                    </i>
+                                </div>
+                            </div>
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                        </div>
+
+                        <div className="qualification__data">
+                            <div></div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                            <div>
+                                <h3 className="qualification__title">POEC C# .net</h3>
+                                <span className="qualification__subtitle">
+                                    Lamanu Versailles
+                                </span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt">
+                                        February 2021 - May 2022
+                                    </i>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div className="qualification__data">
+                            <div>
+                                <h3 className="qualification__title">Web development</h3>
+                                <span className="qualification__subtitle">
+                                    IUT Montbéliard
+                                </span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt">
+                                        September 2011 - September 2014
+                                    </i>
+                                </div>
+                            </div>
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                        </div>
+
+                        <div className="qualification__data">
+                            <div></div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                            <div>
+                                <h3 className="qualification__title">Art & design school</h3>
+                                <span className="qualification__subtitle">
+                                    Atelier de sèvres
+                                </span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt">
+                                        September 2010 - September 2011
+                                    </i>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+export default Qualification
