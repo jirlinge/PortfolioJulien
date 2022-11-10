9f9378c25e615dcc6aabd8c34d7f01b4dbc3ecc2
jirlinge <julienirlinger@gmail.com>
2022-11-10 13:58:57 +0100

    creation des composant skills

---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Skills() {
+    return (
+        <section className="skills section" id="skills">
+            <h2 className="section__title">Skills</h2>
+            <span className="section__subtitle">Technical level</span>
+        </section>
+    )
+}
+export default Skills
