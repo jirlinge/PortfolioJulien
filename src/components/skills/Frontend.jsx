9f893af666c5679f6f25a95aee05d5408f0747fe
jirlinge <julienirlinger@gmail.com>
2022-11-10 14:48:56 +0100

    correction marges aboutPage et creation skillsPage

--- src/components/skills/Frontend.jsx
+++ src/components/skills/Frontend.jsx
@@ -1,30 +1,30 @@
 import React from "react";
 
-function Backend() {
+function Frontend() {
     return (
         <div className="skills__content">
-            <h3 className="skills__title">Backend developer</h3>
+            <h3 className="skills__title">Frontend developer</h3>
 
             <div className="skills__box">
                 <div className="skills__group">
                     <div className="skills__data">
                         <i class='bx bx-badge-check'></i>
                         <div>
-                            <h3 className="skills__name">PHP</h3>
-                            <span className="skills__level">Intermediate</span>
+                            <h3 className="skills__name">HTML</h3>
+                            <span className="skills__level">Advanced</span>
                         </div>
                     </div>
                     <div className="skills__data">
                         <i class='bx bx-badge-check'></i>
                         <div>
-                            <h3 className="skills__name">Python</h3>
-                            <span className="skills__level">Basic</span>
+                            <h3 className="skills__name">CSS</h3>
+                            <span className="skills__level">Advanced</span>
                         </div>
                     </div>
                     <div className="skills__data">
                         <i class='bx bx-badge-check'></i>
                         <div>
-                            <h3 className="skills__name">C#</h3>
+                            <h3 className="skills__name">Javascript</h3>
                             <span className="skills__level">Intermediate</span>
                         </div>
                     </div>
@@ -34,21 +34,21 @@ function Backend() {
                     <div className="skills__data">
                         <i class='bx bx-badge-check'></i>
                         <div>
-                            <h3 className="skills__name">Node.js</h3>
-                            <span className="skills__level">Basic</span>
-                        </div>
-                    </div>
-                    <div className="skills__data">
-                        <i class='bx bx-badge-check'></i>
-                        <div>
-                            <h3 className="skills__name">Laravel</h3>
+                            <h3 className="skills__name">React JS / Native</h3>
                             <span className="skills__level">Intermediate</span>
                         </div>
                     </div>
                     <div className="skills__data">
                         <i class='bx bx-badge-check'></i>
                         <div>
-                            <h3 className="skills__name">Mysql</h3>
+                            <h3 className="skills__name">Bootstrap</h3>
+                            <span className="skills__level">Intermediate</span>
+                        </div>
+                    </div>
+                    <div className="skills__data">
+                        <i class='bx bx-badge-check'></i>
+                        <div>
+                            <h3 className="skills__name">WPF</h3>
                             <span className="skills__level">Intermediate</span>
                         </div>
                     </div>
@@ -57,4 +57,4 @@ function Backend() {
         </div>
     )
 }
-export default Backend
+export default Frontend
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Frontend from "./Frontend";
+import Backend from "./Backend";
+import "./skills.css"
 
 function Skills() {
     return (
         <section className="skills section" id="skills">
             <h2 className="section__title">Skills</h2>
             <span className="section__subtitle">Technical level</span>
+
+            <div className="skills__container container grid">
+                <Frontend></Frontend>
+                <Backend></Backend>
+            </div>
         </section>
     )
 }
