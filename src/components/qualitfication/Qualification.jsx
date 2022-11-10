72fcdffda6f63233445239b61102bc52ac62dd5a
jirlinge <julienirlinger@gmail.com>
2022-11-10 22:50:12 +0100

    finition qualification responsive tres petit format a corriger

--- src/components/qualitfication/Qualification.jsx
+++ src/components/qualitfication/Qualification.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "./qualification.css"
+import { useState } from "react";
 
 function Qualification() {
+    const[toggleState, setToggleState] = useState(1);
+    const toggleTab = (index) => {
+        setToggleState(index);
+    };
     return (
         <section className="qualification section">
             <h2 className="section__title">Qualification</h2>
@@ -9,22 +14,31 @@ function Qualification() {
 
             <div className="qualification__container container">
                 <div className="qualification__tabs">
-                    <div className="qualification__button qualification__active button--flex">
+                    <div className={toggleState === 1 ?
+                         "qualification__button qualification__active button--flex" : 
+                         "qualification__button button--flex"}
+                         onClick={() => toggleTab(1)}
+                         >
                         <i className="uil uil-graduation-cap qualification__icon">
-                        </i>Education
+                        </i>{""}Education
                     </div>
 
-                    <div className="qualification__button button--flex">
+                    <div className={toggleState === 2 ?
+                         "qualification__button qualification__active button--flex" : 
+                         "qualification__button button--flex"}
+                         onClick={() => toggleTab(2)}
+                         >
                         <i className="uil uil-briefcase-alt qualification__icon">
-                        </i>Experience
+                        </i>{""}Experience
                     </div>
                 </div>
 
                 <div className="qualification__sections">
-                    <div className="qualification__content">
+                    <div className={toggleState === 1 ? "qualification__content qualification__content-active"
+                     :"qualification__content"}>
                         <div className="qualification__data">
                             <div>
-                                <h3 className="qualification__title">Concepteur developpeur application</h3>
+                                <h3 className="qualification__title">Concepteur developer application</h3>
                                 <span className="qualification__subtitle">
                                     Lamanu Versailles
                                 </span>
@@ -99,6 +113,106 @@ function Qualification() {
                             </div>
 
                         </div>
+                    </div>
+
+                    <div className={toggleState === 2 ? "qualification__content qualification__content-active"
+                     :"qualification__content"}>
+                        <div className="qualification__data">
+                            <div>
+                                <h3 className="qualification__title">Application developer and designer</h3>
+                                <span className="qualification__subtitle">
+                                    ColibImmo
+                                </span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt">
+                                        May 2021 - June 2022
+                                    </i>
+                                </div>
+                            </div>
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                        </div>
+
+                        <div className="qualification__data">
+                            <div></div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                            <div>
+                                <h3 className="qualification__title">Developer / project manager</h3>
+                                <span className="qualification__subtitle">
+                                L'oeil du jour
+                                </span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt">
+                                        May 2021 - June 2022
+                                    </i>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div className="qualification__data">
+                            <div>
+                                <h3 className="qualification__title">Store manager</h3>
+                                <span className="qualification__subtitle">
+                                Franprix
+                                </span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt">
+                                        September 2018 - August 2020
+                                    </i>
+                                </div>
+                            </div>
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                        </div>
+
+                        <div className="qualification__data">
+                            <div></div>
+
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                            <div>
+                                <h3 className="qualification__title">Webdesigner / communications delegate</h3>
+                                <span className="qualification__subtitle">
+                                    Prohelio
+                                </span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt">
+                                    January 2017 - September 2018
+                                    </i>
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div className="qualification__data">
+                            <div>
+                                <h3 className="qualification__title">Webdesigner</h3>
+                                <span className="qualification__subtitle">
+                                    Agency Ineiaki
+                                </span>
+                                <div className="qualification__calendar">
+                                    <i className="uil uil-calendar-alt">
+                                        September 2014 - September 2015
+                                    </i>
+                                </div>
+                            </div>
+                            <div>
+                                <span className="qualification__rounder"></span>
+                                <span className="qualification__line"></span>
+                            </div>
+                        </div>
+
                     </div>
                 </div>
             </div>
