ad75198bcac05fb4153360b26926184fef8eef64
jirlinge <julienirlinger@gmail.com>
2022-11-14 21:46:05 +0100

    ajout dun nouveau cv

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/about.jpg"
-import CV from "../../assets/JulienCV.pdf"
+import CV from "../../assets/Julienirlingerf4FS.pdf"
 import Info from "./Info";
 
 function About() {
